8ab1f2aa55605a966689e1bb6558c298047d0a46
GameHerobrine <GameHerobrine@users.noreply.github.com>
2024-02-02 15:45:09 +0300

    non vanilla snow layers

--- src/net/skidcode/gh/server/block/impl/SnowLayerBlock.java
+++ src/net/skidcode/gh/server/block/impl/SnowLayerBlock.java
@@ -2,6 +2,7 @@ package net.skidcode.gh.server.block.impl;
 
 import net.skidcode.gh.server.block.Block;
 import net.skidcode.gh.server.block.material.Material;
+import net.skidcode.gh.server.world.World;
 
 public class SnowLayerBlock extends Block{ //TODO some baseclass for non-solid?
 
@@ -10,5 +11,10 @@ public class SnowLayerBlock extends Block{ //TODO some baseclass for non-solid?
 		this.isSolid = false;
 		Block.shouldTick[id] = true;
 	}
-
+	
+	public void onNeighborBlockChanged(World world, int x, int y, int z, int meta) {
+		if(world.getBlockIDAt(x, y-1, z) == 0) {
+			world.setBlock(x, y, z, (byte)0, (byte)0, 3);
+		}
+	}
 }
